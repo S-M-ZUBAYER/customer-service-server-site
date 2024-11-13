68e94b0091717a521480b58c08b347c4047462b0
S-M-ZUBAYER <smzubayer18@gmail.com>
2024-11-13 16:55:14 +0600

    Complete all the api for version control of ios and android

--- routes/version&othersInfo.js
+++ routes/version&othersInfo.js
@@ -30,14 +30,16 @@ router.get('/version', (req, res) => {
 
 router.post('/version/add', (req, res) => {
     const { appVersion, releaseNotes, downloadUrl } = req.body;
+    console.log(appVersion);
 
     const dataToStore = {
         appVersion,
         releaseNotes,
-        downloadUrl
+        downloadUrl,
+        versionMark
     };
 
-    let sql = `INSERT INTO versionInfo (appVersion, releaseNotes, downloadUrl) VALUES (?,?,?)`;
+    let sql = `INSERT INTO versionInfo (appVersion, releaseNotes, downloadUrl) VALUES (?,?,?,?)`;
 
     connection.query(sql, [dataToStore.appVersion, dataToStore.releaseNotes, dataToStore.downloadUrl], function (err, result) {
         if (err) {
@@ -54,17 +56,19 @@ router.post('/version/add', (req, res) => {
 // //create post request to create Shopify all mark link information
 router.put('/version/update/:id', (req, res) => {
     const { id } = req.params;
-    const { appVersion, releaseNotes, downloadUrl } = req.body;
+    const { appVersion, releaseNotes, downloadUrl, versionMark } = req.body;
 
     const dataToUpdate = {
         appVersion,
         releaseNotes,
-        downloadUrl
+        downloadUrl,
+        versionMark
     };
+    console.log(dataToUpdate, id);
 
-    const sql = `UPDATE versionInfo SET appVersion = ?, releaseNotes = ?, downloadUrl = ? WHERE id = ?`;
+    const sql = `UPDATE versionInfo SET appVersion = ?, releaseNotes = ?, downloadUrl = ?,versionMark=? WHERE id = ?`;
 
-    connection.query(sql, [dataToUpdate.appVersion, dataToUpdate.releaseNotes, dataToUpdate.downloadUrl, id], function (err, result) {
+    connection.query(sql, [dataToUpdate.appVersion, dataToUpdate.releaseNotes, dataToUpdate.downloadUrl, dataToUpdate.versionMark, id], function (err, result) {
         if (err) {
             console.error("Error updating data:", err);
             res.status(500).json({ error: "An error occurred while updating data." });
